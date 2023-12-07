0937ea2190b6c180ee7f523fc91c212e1d9d21fe
allembf <allembenfraj@gmail.com>
2023-12-07 12:27:23 +0100

    Write all functions in Test.js

--- getWebApi.js
+++ getWebApi.js
@@ -14,3 +14,10 @@ function getDataFromApi(api){
 }
 const urlApi = 'https://dummyjson.com/products/1';
 getDataFromApi(urlApi);
+
+/* Question: What are the challenges I might face when trying to get back a response? Your answer might include CORS headers)
+
+   Answer : When i make requests from a web page in js, i might face CORS issues. This occurs when the server i'm trying to access hasn't allowed request
+            To solve this problem the server should includes CORS headers, handles requests and allow credentials if needed
+
+ */
